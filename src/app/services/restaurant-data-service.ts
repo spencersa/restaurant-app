e7d201940d1ab842845068ea2d59561114403500
spencersa <spencersa715@gmail.com>
2019-06-27 20:04:08 -0500

    update to use correct function keys for demo

--- src/app/services/restaurant-data-service.ts
+++ src/app/services/restaurant-data-service.ts
@@ -10,35 +10,34 @@ const httpOptions = {
 @Injectable()
 export class RestaurantDataService {
 
-    //apiUrl: string = "https://foodraterapi.azurewebsites.net/api/"; 
-    //apiCode: string = "?code=iLQ8SX1JTaeVx6dku2UsUSTvZR7jZEYk6GxLEMhjpKzZ5Hnz3kiDVQ==";
+    apiUrl: string = "https://foodraterapi.azurewebsites.net/api/";
 
-    apiUrl: string = "http://localhost:7071/api/"; 
-    apiCode: string = "";
+    //apiUrl: string = "http://localhost:7071/api/"; 
+    //apiCode: string = "";
 
     constructor(private http: HttpClient) { }
 
     getAllRestaurants(): Observable<Restaurant[]> {
-        return this.http.get<Restaurant[]>(this.apiUrl + "GetAllRestaurants" + this.apiCode, {responseType: 'json'});
+        return this.http.get<Restaurant[]>(this.apiUrl + "GetAllRestaurants?" + "code=iLQ8SX1JTaeVx6dku2UsUSTvZR7jZEYk6GxLEMhjpKzZ5Hnz3kiDVQ==", {responseType: 'json'});
     }
 
     getRestaurant(rowKey:string): Observable<Restaurant> {
-        return this.http.get<Restaurant>(this.apiUrl + "GetRestaurant?rowKey=" + rowKey);
+        return this.http.get<Restaurant>(this.apiUrl + "GetRestaurant?rowKey=" + rowKey + "&" + "code=Ei5GFV4yBBHLZdQazmv4XgkpREqim0lF0nL/FvEr1ycT7JvoVbagrg==");
     }
 
     addRestaurant(restaurant:Restaurant){
-        return this.http.post<Restaurant>("http://localhost:7071/api/InsertRestaurant", restaurant);
+        return this.http.post<Restaurant>(this.apiUrl + "InsertRestaurant?" + "code=tBaUeU5q0g91C3mENJrGMbGj6fbco3fLk10jJCKte2cEEbvBfeIBrw==", restaurant);
     }
 
     addRestaurantRating(restaurant:Restaurant){
-        return this.http.post<Restaurant>("http://localhost:7071/api/AddRestaurantRating", restaurant);
+        return this.http.post<Restaurant>(this.apiUrl + "AddRestaurantRating?" + "code=Pj8ih79EuQsKds/gHz7q9wTp5WZ5CdExypmHnL1SxLsEEU8hjVmEuQ==", restaurant);
     }
 
     updateRestaurant(restaurant:Restaurant) {
-        return this.http.put("http://localhost:7071/api/UpdateRestaurant", restaurant);
+        return this.http.put(this.apiUrl + "UpdateRestaurant?" + "code=a0kdEH76tK7VUTxOh6djPntgbcSLyD2U8T1C9QEdohfJhUO0Lu4tww==", restaurant);
     }
 
     deleteRestaurant(restaurant:Restaurant) {
-        return this.http.request("delete", "http://localhost:7071/api/DeleteRestaurant", {body: restaurant});
+        return this.http.request("delete", this.apiUrl + "DeleteRestaurant?" + "code=ag/IcUijdBaFfra8BqJ2hPZ84v9yG6ItsKmBaa9bTPmzzieRya7zag==", {body: restaurant});
     }
 }
